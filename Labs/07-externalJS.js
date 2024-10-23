0785a3c13bdf716fd7e2cd80bb8a79b66958d753
Dhana Steen <30987447+deedeesteen@users.noreply.github.com>
2024-10-23 02:52:26 -0500

    Create 07-externalJS.js
    done Oct ‎28, ‎2019

---
+++ Labs/07-externalJS.js
@@ -0,0 +1,10 @@
+//condition testing
+let isSmart = confirm("Are you SMART enough to pass CIT2011?");
+if(isSmart == true)
+{
+	document.write("You are smart enough!");
+}
+else
+{
+	document.write("With practice you can do it!");
+}
