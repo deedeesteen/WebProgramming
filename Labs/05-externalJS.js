cbdadb4cab71f09e64cc346078124595f4393b53
Dhana Steen <30987447+deedeesteen@users.noreply.github.com>
2024-10-23 02:45:36 -0500

    Create 05-externalJS.js
    done Oct ‎28, ‎2019

---
+++ Labs/05-externalJS.js
@@ -0,0 +1,7 @@
+//prompt and alert
+let num1 = prompt("Enter 1st #: ", "");
+let num2 = prompt("Enter 2nd #: ", "");
+
+let result = Number(num1) + Number(num2);
+//document.write(result);
+alert(result);
