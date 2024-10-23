7a62bcf5e84f6c724992e74038d940d42177db2e
Dhana Steen <30987447+deedeesteen@users.noreply.github.com>
2024-10-23 03:35:18 -0500

    Create Javascript Functions and Events.js
    for Javascript Functions and Events.html
    done Nov 18, 2019

---
+++ Labs/Javascript Functions and Events.js
@@ -0,0 +1,13 @@
+function showMessage()
+{
+	let message1 = "Hello, I'm Javascript!"; //Local Variable
+	alert(message1);
+}
+
+let userName = 'John';
+
+function showMessage2()
+{
+	let message = "Hello, I'm Javascript!"; //Local Variable
+	alert(message);
+}
